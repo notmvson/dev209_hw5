68cfa5a95a507e74a672ea3be79c55083ae0701d
notmvson <mason5kong@yahoo.com>
2024-02-04 15:30:22 -0800

    added displayRecipes() function to the view-recipes page

--- main.js
+++ main.js
@@ -8,39 +8,38 @@ let RecipeObject = function (pCategory, pName, pTime, pIngredients, pInstruction
     this.instructions = pInstructions;
 }
 
-RecipeArray.push ( new RecipeObject("Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into bow then pour milk into bowl")  );
-RecipeArray.push ( new RecipeObject("Lunch", "Rice w/ egg", "10 min", "Rice, egg, soy sauce", "Cook rice, fry egg(s), pour soy sauce on top",)  );
-RecipeArray.push ( new RecipeObject("Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwaards")  );
-
-let selectedType = "";
+RecipeArray.push ( new RecipeObject("Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into a bowl followed by milk")  );
+RecipeArray.push ( new RecipeObject("Lunch", "Rice w/ egg", "10 min", "Rice, Egg(s), Soy Sauce", "Cook rice, fry egg(s), pour soy sauce on top",)  );
+RecipeArray.push ( new RecipeObject("Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwards")  );
 
 // code runs immediately
 //================================================================
 
 // runs  when dom is loaded
-$(document).on("pagecreate", function() {
+$(document).on("pageinit", "#add-recipe", function() {
     createList();
 
     $("#buttonAdd").on("click", function() {
+        const category = $("#recipe-category").val();
         const name = $("#recipe-name").val();
         const time = $("#recipe-time").val();
         const ingredients = $("#recipe-ingredients").val();
         const instructions = $("#recipe-instructions").val();
 
         if (name && time && ingredients && instructions) {
-            RecipeArray.push(new RecipeObject(selectedType, name, time, ingredients, instructions));
+            RecipeArray.push(new RecipeObject(category, name, time, ingredients, instructions));
 
-            $("#recipe-name, #recipe-time, #recipe-ingredients, #recipe-instructions").val("");
+            $("#recipe-category, #recipe-name, #recipe-time, #recipe-ingredients, #recipe-instructions").val("");
             createList();
         } else {
             alert("Please fill in all fields before adding a recipe.");
         }
     });
+});
 
-    $(document).bind("change", "#recipe-category", function (event, ui) {
-        selectedType = $("#recipe-category").val();
-    });
-})
+$(document).on("pageinit", "#view-recipes", function() {
+    displayRecipes();
+});
 
 
 
@@ -54,7 +53,18 @@ function createList() {
     RecipeArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
           // added data-role="listview" to the ul in the html
-        li.innerHTML = element.category + " - " + element.name + " (" + element.time + "): " + element.ingredients + " " + element.instructions;
+        li.innerHTML = "<b>" + element.name + " [" + element.category + "] (" + element.time + ") </b><br> Ingredients: " + element.ingredients + " <br> Instructions: " + element.instructions;
         myul.appendChild(li);
     });
 };
+
+function displayRecipes() {
+    var myul = document.getElementById("recipe-list");
+    myul.innerHTML = "";
+
+    RecipeArray.forEach(function(element) {
+        var li = document.createElement('li');
+        li.innerHTML = "<b>" + element.name + " [" + element.category + "] (" + element.time + ") </b><br> <i>Ingredients:</i> " + element.ingredients + " <br> <i>Instructions:</i> " + element.instructions;
+        myul.appendChild(li);
+    });
+}
