a66cb827dce21d7c2a9223bc0747f0173ab65c31
notmvson <mason5kong@yahoo.com>
2024-02-20 21:32:09 -0800

    added 4th page

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let RecipeArray = [];
 
-let RecipeObject = function(pCategory, pName, pTime, pIngredients, pInstructions) {
+let RecipeObject = function(pID, pCategory, pName, pTime, pIngredients, pInstructions) {
+    this.ID = pID;
+    this.ID = RecipeArray.length + 1;
     this.category = pCategory;
     this.name = pName;
     this.time = pTime;
@@ -8,14 +10,18 @@ let RecipeObject = function(pCategory, pName, pTime, pIngredients, pInstructions
     this.instructions = pInstructions;
 }
 
-RecipeArray.push(new RecipeObject("Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into a bowl followed by milk"));
-RecipeArray.push(new RecipeObject("Lunch", "Rice w/ egg", "10 min", "Rice, Egg(s), Soy Sauce", "Cook rice, fry egg(s), pour soy sauce on top"));
-RecipeArray.push(new RecipeObject("Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwards"));
+RecipeArray.push(new RecipeObject(1, "Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into a bowl followed by milk"));
+RecipeArray.push(new RecipeObject(2, "Lunch", "Rice w/ egg", "10 min", "Rice, Egg(s), Soy Sauce", "Cook rice, fry egg(s), pour soy sauce on top"));
+RecipeArray.push(new RecipeObject(3, "Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwards"));
+
+$(document).on("pagebeforeshow", "#view-recipes", function() {
+    createList(); // Update the recipe list on pageinit
+});
 
 $(document).on("pagebeforeshow", "#add-recipe", function() {
-    createList();
 
     $("#buttonAdd").on("click", function() {
+        const ID = RecipeArray.length;
         const category = $("#recipe-category").val();
         const name = $("#recipe-name").val();
         const time = $("#recipe-time").val();
@@ -23,8 +29,8 @@ $(document).on("pagebeforeshow", "#add-recipe", function() {
         const instructions = $("#recipe-instructions").val();
 
         if (category && name && time && ingredients && instructions) {
-            RecipeArray.push(new RecipeObject(category, name, time, ingredients, instructions));
-
+            RecipeArray.push(new RecipeObject(ID, category, name, time, ingredients, instructions));
+            document.location.href = "index.html#view-recipes"
             $("#recipe-category, #recipe-name, #recipe-time, #recipe-ingredients, #recipe-instructions").val("");
             createList();
         } else {
@@ -33,45 +39,48 @@ $(document).on("pagebeforeshow", "#add-recipe", function() {
     });
 });
 
-$(document).on("pagebeforeshow", "#view-recipes", function() {
-    createList(); // Update the recipe list on pageinit
+$(document).on("pagebeforeshow", "#details", function(event) {
+    let localID = localStorage.getItem("parm");
+    RecipeArray = JSON.parse(localStorage.getItem("RecipeArray"));
+    console.log(RecipeArray[localID - 1]);
 
-    // Handle click events on recipe list items
-    $("#recipe-list").on("click", "li", function() {
-        var index = $(this).index();
-        var recipe = RecipeArray[index];
-
-        // Perform a unique action for each recipe
-        switch (recipe.name) {
-            case "Cereal":
-                window.open("https://www.wikihow.com/Eat-a-Bowl-of-Cereal", "_blank");
-                break;
-            case "Rice w/ egg":
-                window.open("https://christieathome.com/blog/gyeran-bap-korean-egg-rice/", "_blank");
-                break;
-            case "PB&J":
-                window.open("https://www.wikihow.com/Make-a-Peanut-Butter-and-Jelly-Sandwich", "_blank");
-                break;
-            default:
-                // Handle default action or leave empty
-                break;
-        }
-    });
-});
+    document.getElementById("oneName").innerHTML = "Recipe Name: " + RecipeArray[localID - 1].name;
+    document.getElementById("oneCategory").innerHTML = "Category: " + RecipeArray[localID - 1].category;
+    document.getElementById("oneTime").innerHTML = "Cook Time: " + RecipeArray[localID - 1].time;
+    document.getElementById("oneIngredients").innerHTML = "Ingredients: " + RecipeArray[localID - 1].ingredients;
+    document.getElementById("oneInstructions").innerHTML = "Instructions: " + RecipeArray[localID - 1].instructions;
+})
 
 function createList() {
     var myul = $("#recipe-list");
     myul.empty();
 
     RecipeArray.forEach(function(element, index) {
-        var li = $("<li>").html("<b>" + element.name + " [" + element.category + "] (" + element.time + ") </b><br> <i>Ingredients:</i> " + element.ingredients + " <br> <i>Instructions:</i> " + element.instructions);
+        var li = $("<li>").html("<b>" + element.name + " [" + element.category + "] (" + element.time + ") </b><br> <i>Ingredients:</i> " + element.ingredients);
+        li.addClass("oneRecipe");
+        li.attr("data-parm", element.ID);
         var deleteButton = $("<button>").text("Delete").addClass("delete-button").data("index", index);
         li.append(deleteButton);
         myul.append(li);
     });
 
-    //delete buttons
-    $(".delete-button").on("click", function() {
+    var liList = document.getElementsByClassName("oneRecipe");
+    let newRecipeArray = Array.from(liList);
+
+    newRecipeArray.forEach(function(element) {
+        element.addEventListener ("click", function() {
+            var parm = this.getAttribute("data-parm");
+            localStorage.setItem("parm", parm);
+
+            let stringRecipeArray = JSON.stringify(RecipeArray);
+            localStorage.setItem("RecipeArray", stringRecipeArray);
+
+            document.location.href = "index.html#details";
+
+        })
+    })
+
+    $(".delete-button").on("click", function() { //delete buttons
         var indexToRemove = $(this).data("index");
         RecipeArray.splice(indexToRemove, 1); // Remove the recipe at the specified index
         createList();
