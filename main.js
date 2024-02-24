8b94934d58a06aaef3a58b9d9fea45b3581fdca4
notmvson <mason5kong@yahoo.com>
2024-02-23 17:05:47 -0800

    Updated ID element

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let RecipeArray = [];
 
-let RecipeObject = function(pID, pCategory, pName, pTime, pIngredients, pInstructions) {
-    this.ID = pID;
-    this.ID = RecipeArray.length + 1;
+let RecipeObject = function(pCategory, pName, pTime, pIngredients, pInstructions) {
+    this.ID = Math.random().toString(16).slice(5);
     this.category = pCategory;
     this.name = pName;
     this.time = pTime;
@@ -10,26 +9,23 @@ let RecipeObject = function(pID, pCategory, pName, pTime, pIngredients, pInstruc
     this.instructions = pInstructions;
 }
 
-RecipeArray.push(new RecipeObject(1, "Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into a bowl followed by milk"));
-RecipeArray.push(new RecipeObject(2, "Lunch", "Rice w/ egg", "10 min", "Rice, Egg(s), Soy Sauce", "Cook rice, fry egg(s), pour soy sauce on top"));
-RecipeArray.push(new RecipeObject(3, "Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwards"));
-
-$(document).on("pagebeforeshow", "#view-recipes", function() {
-    createList(); // Update the recipe list on pageinit
-});
+// 3 Example Recipes
+RecipeArray.push(new RecipeObject("Breakfast", "Cereal", "3 min", "Cereal and Milk", "Pour cereal into a bowl followed by milk"));
+RecipeArray.push(new RecipeObject("Lunch", "Rice w/ egg", "10 min", "Rice, Egg(s), Soy Sauce", "Cook rice, fry egg(s), pour soy sauce on top"));
+RecipeArray.push(new RecipeObject("Breakfast", "PB&J", "5 min", "Bread, Peanut Butter, Jelly", "Spread peanut butter on one side of both pieces of bread, spread jelly on top of the peanut butter, put the bread together w pb&j facing inwards"));
 
 $(document).on("pagebeforeshow", "#add-recipe", function() {
 
     $("#buttonAdd").on("click", function() {
-        const ID = RecipeArray.length;
         const category = $("#recipe-category").val();
         const name = $("#recipe-name").val();
         const time = $("#recipe-time").val();
         const ingredients = $("#recipe-ingredients").val();
         const instructions = $("#recipe-instructions").val();
 
-        if (category && name && time && ingredients && instructions) {
-            RecipeArray.push(new RecipeObject(ID, category, name, time, ingredients, instructions));
+        // Validates forms are filled out
+        if (category && name && time && ingredients && instructions) { 
+            RecipeArray.push(new RecipeObject(category, name, time, ingredients, instructions));
             document.location.href = "index.html#view-recipes"
             $("#recipe-category, #recipe-name, #recipe-time, #recipe-ingredients, #recipe-instructions").val("");
             createList();
@@ -39,17 +35,29 @@ $(document).on("pagebeforeshow", "#add-recipe", function() {
     });
 });
 
+$(document).on("pagebeforeshow", "#view-recipes", function() {
+    createList(); // Updates the view-recipes page
+});
+
 $(document).on("pagebeforeshow", "#details", function(event) {
     let localID = localStorage.getItem("parm");
+    let pointer = GetObjectPointer(localID);
     RecipeArray = JSON.parse(localStorage.getItem("RecipeArray"));
-    console.log(RecipeArray[localID - 1]);
 
-    document.getElementById("oneName").innerHTML = RecipeArray[localID - 1].name + "<hr>";
-    document.getElementById("oneCategory").innerHTML = "Category: " + RecipeArray[localID - 1].category;
-    document.getElementById("oneTime").innerHTML = "Cook Time: " + RecipeArray[localID - 1].time;
-    document.getElementById("oneIngredients").innerHTML = "Ingredients: " + RecipeArray[localID - 1].ingredients;
-    document.getElementById("oneInstructions").innerHTML = "Instructions: " + RecipeArray[localID - 1].instructions;
+    document.getElementById("oneName").innerHTML = RecipeArray[pointer].name + "<hr>";
+    document.getElementById("oneCategory").innerHTML = "Category: " + RecipeArray[pointer].category;
+    document.getElementById("oneTime").innerHTML = "Cook Time: " + RecipeArray[pointer].time;
+    document.getElementById("oneIngredients").innerHTML = "Ingredients: " + RecipeArray[pointer].ingredients;
+    document.getElementById("oneInstructions").innerHTML = "Instructions: " + RecipeArray[pointer].instructions;
 })
+
+function GetObjectPointer(whichID) {
+    for(i = 0; i < RecipeArray.length; i++) {
+        if(RecipeArray[i].ID == whichID) {
+            return i;
+        }
+    }
+}
 
 function createList() {
     var myul = $("#recipe-list");
